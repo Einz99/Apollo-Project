6123e203fa9e9d0c6a88642bcc2159dd05bef9ee
Einz99 <ronaldvillarde999@gmail.com>
2024-12-06 21:04:40 +0800

    Fix Contact, Finishes layout of Artists

--- js/artist.js
+++
@@ -1,6 +0,0 @@
-const button = document.querySelector('a.button')
-
-button.addEventListener('click', () => {
-    sessionStorage.setItem('fromShop', false);
-    window.location.href="artList.html"
-})
